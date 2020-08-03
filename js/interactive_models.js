395c615405f01907d2898de3b21ecac9a2a52201
Marcel Tunnissen <marcelteun@gmail.com>
2020-08-03 10:55:02 +0200

    Add page for tetrahedron compounds (central freedom)
    Change-Id: I2600c85e9123b41a8f5f33eabf3e5c179d391920

--- js/interactive_models.js
+++ js/interactive_models.js
@@ -1,4 +1,5 @@
 function show(model, cam_dist) {
+  // static show: off file and no slide-bar
   document.getElementById("model").style.display = "block";
   document.getElementById("model").style.initialised = false;
   slider = document.getElementById("rot_angle")
@@ -8,18 +9,31 @@ function show(model, cam_dist) {
   if (!cam_dist) {cam_dist = 10;}
   draw_shape(model, 'model_canvas', cam_dist);
 }
-function ishow(model, axis, rmin, rmax, rstep, cam_dist) {
+function ishow(model, cam_dist, axis, rmin, rmax, rstep) {
+  // interactive show: with slide-bar
   document.getElementById("model").style.display = "block";
   document.getElementById("model").style.initialised = false;
   var slider = document.getElementById("rot_angle");
   slider.hidden = false;
-  slider.min = rmin;
-  slider.max = rmax;
-  slider.step = rstep;
+  // either define one or define all
+  if (axis === undefined || rmin === undefined || rmax === undefined) {
+    slider.min = model.angle_domain[0];
+    slider.max = model.angle_domain[1];
+    slider.rot_axis = model.rot_axis
+  } else {
+    slider.min = rmin;
+    slider.max = rmax;
+    slider.rot_axis = axis
+  }
+  if (rstep === undefined) {
+    slider.step = 0.001;
+  } else {
+    slider.step = rstep;
+  }
   if (!cam_dist) {cam_dist = 10;}
   var shape = new OrbitShape(model.descr, model.isoms, 'model_canvas', cam_dist, {has_concave_faces: false});
   slider.oninput = function() {
-    shape.rotate_descriptive(axis, this.value);
+    shape.rotate_descriptive(this.rot_axis, this.value);
   }
   slider.value = 0;
 }
