39f062248a293a6e9cad7fbeb33d16afdea985e5
Marcel Tunnissen <marcelteun@gmail.com>
2020-10-25 11:36:18 +0100

    Compounds: add title and angle to slidebar
    Changes the style to a grey opaque div with possible title and angle.
    
    Change-Id: I0e3b1aa1fe1a31e2d1f644c80a3da95dcba3ba6f

--- js/interactive_models.js
+++ js/interactive_models.js
@@ -1,29 +1,58 @@
+function show_with_title(model_name, model, cam_dist) {
+  show(model, cam_dist);
+  var title = document.getElementById("canvas_title");
+  if (title) {
+    title.innerHTML = model_name;
+  }
+}
 function show(model, cam_dist) {
   // static show: off file and no slide-bar
   document.getElementById("model").style.display = "block";
   document.getElementById("model").style.initialised = false;
-  slider = document.getElementById("rot_angle")
+  var slider = document.getElementById("rot_angle")
+  var angle_text = document.getElementById("angle_text");
+  if (angle_text) {
+    angle_text.style.display = "none";
+  }
   if (slider != null) {
     slider.hidden = true;
   }
   if (!cam_dist) {cam_dist = 10;}
   draw_shape(model, 'model_canvas', cam_dist);
+  var title = document.getElementById("canvas_title");
+  if (title) {
+    title.innerHTML = "";
+  }
+}
+function ishow_with_title(model_name, model, cam_dist, axis, rmin, rmax, rstep) {
+  ishow(model, cam_dist, axis, rmin, rmax, rstep)
+  var title = document.getElementById("canvas_title");
+  if (title) {
+    title.innerHTML = model_name;
+  }
 }
 function ishow(model, cam_dist, axis, rmin, rmax, rstep) {
   // interactive show: with slide-bar
   document.getElementById("model").style.display = "block";
   document.getElementById("model").style.initialised = false;
   var slider = document.getElementById("rot_angle");
+  var angle = document.getElementById("angle");
+  var angle_text = document.getElementById("angle_text");
   slider.hidden = false;
+  slider.value = 0;
+  if (angle_text) {
+    angle_text.style.display = "block";
+    angle.innerHTML = 0;
+  }
   // either define one or define all
   if (axis === undefined || rmin === undefined || rmax === undefined) {
     slider.min = model.angle_domain[0];
     slider.max = model.angle_domain[1];
-    slider.rot_axis = model.rot_axis
+    slider.rot_axis = model.rot_axis;
   } else {
     slider.min = rmin;
     slider.max = rmax;
-    slider.rot_axis = axis
+    slider.rot_axis = axis;
   }
   if (rstep === undefined) {
     slider.step = 0.001;
@@ -34,8 +63,15 @@ function ishow(model, cam_dist, axis, rmin, rmax, rstep) {
   var shape = new OrbitShape(model.descr, model.isoms, 'model_canvas', cam_dist, {has_concave_faces: false});
   slider.oninput = function() {
     shape.rotate_descriptive(this.rot_axis, this.value);
+    var angle_deg = this.value * 180 / Math.PI;
+    if (angle_text) {
+      angle.innerText = Math.round((angle_deg + Number.EPSILON) * 10) / 10;
+    }
   }
-  slider.value = 0;
+  var title = document.getElementById("canvas_title");
+  if (title) {
+    title.innerText = "";
+  }
 }
 function hide() {
   if (document.getElementById("model").style.initialised) {
