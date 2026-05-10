d7f2d527e9d4516fa6028e80a073313f8b302ca8
Marcel Tunnissen <marcelteun@gmail.com>
2026-05-11 01:41:42 +0200

    Tetartoids: add interactive caltrop

--- polyh/tetartoids/interactive_tetartoids.js
+++ polyh/tetartoids/interactive_tetartoids.js
@@ -12,119 +12,125 @@ function tetartoid_vertices(a, b, c) {
     [-e2, e2, e2],
   ];
 }
-var pyrito = new Object();
-pyrito.descr = new Object();
 var tau = (Math.sqrt(5) + 1) / 2
-pyrito.a = 0;
-pyrito.b = tau + 1
-pyrito.c = tau
-pyrito.descr.Vs = tetartoid_vertices(pyrito.a, pyrito.b, pyrito.c);
-pyrito.descr.Fs = [[0, 1, 2, 3, 4]];
-pyrito.descr.transform = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
-pyrito.isoms = [
-[
-  [-1, 0, 0, 0],
-  [0, -1, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 0, 1, 0],
-  [-1, 0, 0, 0],
-  [0, -1, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 0, 1, 0],
-  [1, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [1, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 1]
-],[
-  [1, 0, 0, 0],
-  [0, -1, 0, 0],
-  [0, 0, -1, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 0, -1, 0],
-  [1, 0, 0, 0],
-  [0, -1, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 1, 0, 0],
-  [0, 0, -1, 0],
-  [-1, 0, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [-1, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, -1, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 1, 0, 0],
-  [0, 0, 1, 0],
-  [1, 0, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [0, -1, 0, 0],
-  [0, 0, 1, 0],
-  [-1, 0, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [0, -1, 0, 0],
-  [0, 0, -1, 0],
-  [1, 0, 0, 0],
-  [0, 0, 0, 1]
-],[
-  [0, 0, -1, 0],
-  [-1, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1]
-],];
-pyrito.isoms[0].col = [64, 104, 224];
-pyrito.isoms[1].col = [64, 104, 224];
-pyrito.isoms[2].col = [153, 204, 49];
-pyrito.isoms[3].col = [153, 204, 49];
-pyrito.isoms[4].col = [220, 159, 220];
-pyrito.isoms[5].col = [220, 159, 220];
-pyrito.isoms[6].col = [254, 214, 0];
-pyrito.isoms[7].col = [254, 214, 0];
-pyrito.isoms[8].col = [153, 204, 49];
-pyrito.isoms[9].col = [64, 104, 224];
-pyrito.isoms[10].col = [220, 159, 220];
-pyrito.isoms[11].col = [254, 214, 0];
+var tetar = new Object();
+tetar.descr = new Object();
+tetar.descr.Fs = [[0, 1, 2, 3, 4]];
+tetar.descr.transform = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+tetar.isoms = [
+  [
+    [-1, 0, 0, 0],
+    [0, -1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 0, 1, 0],
+    [-1, 0, 0, 0],
+    [0, -1, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 0, 1, 0],
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ],[
+    [1, 0, 0, 0],
+    [0, -1, 0, 0],
+    [0, 0, -1, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 0, -1, 0],
+    [1, 0, 0, 0],
+    [0, -1, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 1, 0, 0],
+    [0, 0, -1, 0],
+    [-1, 0, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [-1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, -1, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, -1, 0, 0],
+    [0, 0, 1, 0],
+    [-1, 0, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, -1, 0, 0],
+    [0, 0, -1, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1]
+  ],[
+    [0, 0, -1, 0],
+    [-1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1]
+  ],
+];
+tetar.isoms[0].col = [64, 104, 224];
+tetar.isoms[1].col = [64, 104, 224];
+tetar.isoms[2].col = [153, 204, 49];
+tetar.isoms[3].col = [153, 204, 49];
+tetar.isoms[4].col = [220, 159, 220];
+tetar.isoms[5].col = [220, 159, 220];
+tetar.isoms[6].col = [254, 214, 0];
+tetar.isoms[7].col = [254, 214, 0];
+tetar.isoms[8].col = [153, 204, 49];
+tetar.isoms[9].col = [64, 104, 224];
+tetar.isoms[10].col = [220, 159, 220];
+tetar.isoms[11].col = [254, 214, 0];
 
-ModelPlay.prototype.tetar_show = function(model, rmin, rmax, title="", rstep=undefined) {
+ModelPlay.prototype.tetar_show = function(a, b, c, rmin, rmax, title="", scale=false, rstep=undefined) {
   // interactive show: with slide-bar
+  tetar.a = a;
+  tetar.b = b;
+  tetar.c = c;
+  tetar.descr.Vs = tetartoid_vertices(tetar.a, tetar.b, tetar.c);
   this.nodes.area.style.display = "block";
   this.nodes.area.style.initialised = false;
 
   this.nodes.slider.hidden = false;
-  this.nodes.slider.value = Math.sqrt(Math.sqrt(tau));
   this.nodes.angle_text.style.display = "block";
-  // either define one or define all
-  if (rmin === undefined || rmax === undefined) {
-    this.nodes.slider.min = model.angle_domain[0];
-    this.nodes.slider.max = model.angle_domain[1];
-  } else {
-    this.nodes.slider.min = rmin;
-    this.nodes.slider.max = rmax;
-  }
+  this.nodes.slider.min = rmin;
+  this.nodes.slider.max = rmax;
   this.nodes.slider.player = this;
   if (rstep === undefined) {
     this.nodes.slider.step = 0.001;
   } else {
     this.nodes.slider.step = rstep;
   }
-  var shape = new OrbitShape(model.descr, model.isoms, this.nodes.canvas, {has_concave_faces: true});
+  var shape = new OrbitShape(tetar.descr, tetar.isoms, this.nodes.canvas, {has_concave_faces: true});
+
+  this.nodes.slider.my_scale = scale;
+  if (scale) {
+    this.nodes.slider.value = Math.sqrt(Math.sqrt(c));
+  } else {
+    this.nodes.slider.value = c;
+  }
   this.nodes.slider.oninput = function() {
-    shape.base.Vs = tetartoid_vertices(model.a, model.b, parseFloat(this.value) ** 4);
+    var c = parseFloat(this.value);
+    console.log("Got input value", c);
+    if (this.my_scale) {
+      c = c ** 4;
+    }
+    shape.base.Vs = tetartoid_vertices(tetar.a, tetar.b, c);
     shape.rescale();
     shape.triangulate();
     shape.paint();
   }
-  this.nodes.slider.oninput()
+  this.nodes.slider.oninput();
   this.nodes.title.innerHTML = title;
 }
